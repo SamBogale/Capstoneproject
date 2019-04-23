f00ae33adb87a8abcef261954219b51c95e600a0
Sam bog <sam bogale>
2019-04-23 11:27:22 -0500

    build npx parcel

--- dist/Capstoneproject.e31bb0bc.js
+++ dist/Capstoneproject.e31bb0bc.js
@@ -21536,7 +21536,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "51031" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "50193" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
